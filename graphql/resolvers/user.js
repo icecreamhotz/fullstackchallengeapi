5dc83b045f9e7e417270ef429ab16fe1b6f67044
l3est <best_dota@hotmail.com>
2019-06-04 12:11:49 +0700

    make it better

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -15,25 +15,27 @@ module.exports = {
           let socketIO = global.socketIO;
           await User.findOne({ telephone: args.locker.telephone }).then(
             async user => {
-              const data = {
+              let data = {
                 _id: args.locker._id,
                 income: args.locker.income,
                 timeout: args.locker.timeout,
-                status: args.locker.status,
-                user: user._id
+                status: args.locker.status
               };
               if (user) {
+                data = { ...data, user: user._id };
                 const lockers = await locker.rentLocker(data);
                 socketIO.sockets.emit("lockers", lockers);
                 resolve(lockers);
               }
+              const userId = new mongoose.Types.ObjectId();
               const createUser = new User({
-                _id: new mongoose.Types.ObjectId(),
-                telephone: args.user.telephone
+                _id: userId,
+                telephone: args.locker.telephone
               });
               createUser
                 .save()
                 .then(async () => {
+                  data = { ...data, user: userId };
                   const lockers = await locker.rentLocker(data);
                   socketIO.sockets.emit("lockers", lockers);
                   resolve(lockers);
